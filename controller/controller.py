edf4174377f8a59eb9b723df641602c77740b441
benzaeske <32627698+benzaeske@users.noreply.github.com>
2025-12-17 13:00:34 -0700

    abstract fps screen printing to its own function

--- controller/controller.py
+++ controller/controller.py
@@ -82,6 +82,7 @@ class GameController:
         model_update_time = time.time() - model_update_time
         view_update_time = time.time()
         self.draw_background()
+        self.draw_fps_menu()
         self.draw_game_entities()
         self.view.update_screen()
         view_update_time = time.time() - view_update_time
@@ -107,6 +108,7 @@ class GameController:
                 ),
             )
 
+    def draw_fps_menu(self) -> None:
         self.view.print_info_to_screen(
             self.clock.get_fps(),
             int(self.model.player.position.x),
