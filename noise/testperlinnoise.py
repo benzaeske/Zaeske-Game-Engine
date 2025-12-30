ab3a856b744e4d70f157e8066f371742c44e1495
Ben Zaeske <32627698+benzaeske@users.noreply.github.com>
2025-12-30 00:43:01 -0700

    playing with noise

--- noise/testperlinnoise.py
+++ noise/testperlinnoise.py
@@ -1,49 +1,97 @@
-import sys
+import random
+import time
 
 import pygame
+from perlin_noise import PerlinNoise
 from perlin_numpy import generate_perlin_noise_2d
 from pygame import Surface
 from pygame.time import Clock
 
-import noise
+grid_square_dimension: float = 8.0
+grid_width: int = 128
+grid_height: int = 128
 
 
-def attempt_1():
-    pygame.init()
-
-    grid_square_dimension: float = 8.0
-    grid_width: int = 128
-    grid_height: int = 128
-
-    screen = pygame.display.set_mode(
-        (grid_width * grid_square_dimension, grid_height * grid_square_dimension)
-    )
-    screen.fill((0, 0, 0))
-    pygame.display.update()
-    pygame.display.set_caption("Perlin Noise")
-
-    noise_array = generate_perlin_noise_2d((grid_width, grid_height), (2, 2))
-    background_tiles = [
+def initialize_noise_grid() -> list[list[Surface]]:
+    return [
         [
             Surface((grid_square_dimension, grid_square_dimension))
-            for x in range(grid_width)
+            for _ in range(grid_width)
         ]
-        for y in range(grid_height)
+        for _ in range(grid_height)
     ]
+
+
+def attempt_1() -> list[list[Surface]]:
+    noise_array = generate_perlin_noise_2d((grid_width, grid_height), (2, 2))
+    background_tiles = initialize_noise_grid()
     for row in range(grid_height):
         for col in range(grid_width):
             noise = int(noise_array[row][col] * 50)
             background_tiles[row][col].fill((0, 50 + noise, 140 + (noise * 2)))
+    return background_tiles
 
+
+def attempt_2() -> list[list[Surface]]:
+    seed: int = random.randint(1, 10**5)
+    # Inputs
+    octaves: int = 1
+
+    # Instantiate noise generator
+    noise_generator: PerlinNoise = PerlinNoise(octaves, seed)
+    print("generator created with seed:", noise_generator.seed)
+
+    background_tiles: list[list[Surface]] = initialize_noise_grid()
+
+    min_noise: float = 1
+    max_noise: float = -1
+    # Loop and create tiles
+    for y in range(grid_height):
+        for x in range(grid_width):
+            nx = x / grid_height
+            ny = y / grid_width
+            noise = noise_generator.noise((nx, ny))
+            min_noise = min(noise, min_noise)
+            max_noise = max(noise, max_noise)
+            """
+            noise = (noise / 2) + 0.5
+            noise = noise * noise
+            noise = noise * 255
+            noise = 255 if noise < 0 else 1
+            """
+            noise = noise * 127
+            background_tiles[y][x].fill((0, 30, 127 + (noise * 1)))
+    print("min:", min_noise)
+    print("max:", max_noise)
+
+    return background_tiles
+
+
+def main():
+    # Load
+    generation_load_time: float = time.time()
+    background_tiles: list[list[Surface]] = attempt_2()
+    generation_load_time = time.time() - generation_load_time
+    print("noise generation load time (seconds):", generation_load_time)
+    # Pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode(
+        (grid_width * grid_square_dimension, grid_height * grid_square_dimension)
+    )
+    pygame.display.set_caption("Perlin Noise")
     clock: Clock = pygame.time.Clock()
     running = True
+    # Draw loop
     while running:
+        # Check for terminate
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+        # Draw noise
+        draw_time: float = time.time()
         for row in range(grid_height):
             for col in range(grid_width):
                 screen.blit(
@@ -51,66 +99,9 @@ def attempt_1():
                     (grid_square_dimension * col, grid_square_dimension * row),
                 )
         pygame.display.flip()
-        clock.tick(60)
-    pygame.quit()
-    sys.exit()
+        draw_time = time.time() - draw_time
+        print("noise draw time (seconds):", draw_time)
+        clock.tick(30)
 
 
-attempt_1()
-
-
-def attempt_2():
-    octaves = 2  # Number of layers of detail
-    persistence = (
-        0.5  # How much each octave contributes to the overall shape (amplitude)
-    )
-    lacunarity = 2.0  # How much detail is added at each octave (frequency)
-
-    pygame.init()
-
-    grid_square_dimension: float = 8.0
-    grid_width: int = 128
-    grid_height: int = 128
-
-    screen = pygame.display.set_mode(
-        (grid_width * grid_square_dimension, grid_height * grid_square_dimension)
-    )
-    screen.fill((0, 0, 0))
-    pygame.display.update()
-    pygame.display.set_caption("Perlin Noise")
-
-    background_tiles = [
-        [
-            Surface((grid_square_dimension, grid_square_dimension))
-            for x in range(grid_width)
-        ]
-        for y in range(grid_height)
-    ]
-    for y in range(grid_height):
-        for x in range(grid_width):
-            n = noise.pnoise2(
-                (x / grid_width) - 0.5,
-                (y / grid_height) - 0.5,
-                base=base,
-                repeatx=grid_width,
-            )
-            n = (n / 2) + 0.5
-            n = n * 255
-            background_tiles[y][x].fill((0, 0, n))
-
-    clock: Clock = pygame.time.Clock()
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        for y in range(grid_height):
-            for x in range(grid_width):
-                screen.blit(
-                    background_tiles[y][x],
-                    (grid_square_dimension * x, grid_square_dimension * y),
-                )
-        pygame.display.flip()
-        clock.tick(60)
-    pygame.quit()
-    sys.exit()
+main()
