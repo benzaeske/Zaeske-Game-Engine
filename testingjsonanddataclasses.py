84a859fdb160572441fe8ce1cf5b059ac4105d6b
Ben <32627698+benzaeske@users.noreply.github.com>
2026-04-10 17:18:23 +0900

    removing testing files

--- testingjsonanddataclasses.py
+++
@@ -1,8 +0,0 @@
-from model.entities.enemies.enemyconfig import EnemyConfig
-from model.entities.fish.fishconfig import FishConfig
-
-red_fish: FishConfig = FishConfig.from_file('assets/entityconfigurations/red_fish.json')
-red_jellyfish: EnemyConfig = EnemyConfig.from_file('assets/entityconfigurations/red_jellyfish.json')
-
-print(red_fish.entity_type)
-print(red_jellyfish.entity_type)
